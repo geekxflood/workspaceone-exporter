afe353387ba66292f11c07958ffd1c167ed11510
Christophe Rime <christopherime@me.com>
2023-12-31 11:22:44 +0100

    Basic restructure

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/geekxflood/workspaceone-exporter/internal/httpclient"
@@ -14,6 +15,10 @@ var (
 	port        string
 	insecure    bool
 	defaultLgid int
+	ws1URL      string
+	ws1Interval string
+	tagParsing  bool
+	tagFilter   string
 )
 
 var rootCmd = &cobra.Command{
@@ -33,6 +38,10 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", "9100", "Port to run the HTTP server on")
 	rootCmd.PersistentFlags().BoolVarP(&insecure, "insecure", "i", false, "Ignore TLS for self-signed certificates")
 	rootCmd.PersistentFlags().IntVarP(&defaultLgid, "default-lgid", "d", 0, "Default LGID value to use if not provided in query")
+	rootCmd.PersistentFlags().StringVar(&ws1URL, "ws1-url", "", "WorkspaceOne UEM base API URL endpoint")
+	rootCmd.PersistentFlags().StringVar(&ws1Interval, "ws1-interval", "60", "Interval between each WS1 check to its enrolled devices in minutes")
+	rootCmd.PersistentFlags().BoolVar(&tagParsing, "tag-parsing", false, "Enable or disable the tag parsing")
+	rootCmd.PersistentFlags().StringVar(&tagFilter, "tag-filter", "", "String to filter Tag by it")
 }
 
 func runServer(cmd *cobra.Command, args []string) {
@@ -64,9 +73,32 @@ func metricsHandler(w http.ResponseWriter, r *http.Request, client *http.Client)
 		}
 	}
 
-	// Example: Update your metrics based on the lgid
-	// This is where you would call your internal API functions using the client
-	// and update your Prometheus metrics
+	if intervalParam := queryParams.Get("ws1-interval"); intervalParam != "" {
+		ws1Interval = intervalParam
+	}
+
+	if urlParam := queryParams.Get("ws1-url"); urlParam != "" {
+		if _, err := url.ParseRequestURI(urlParam); err != nil {
+			log.Printf("Invalid ws1-url parameter: %v", err)
+			http.Error(w, "Invalid ws1-url parameter", http.StatusBadRequest)
+			return
+		}
+		ws1URL = urlParam
+	}
+
+	if parsingParam := queryParams.Get("tag-parsing"); parsingParam != "" {
+		parsedValue, err := strconv.ParseBool(parsingParam)
+		if err == nil {
+			tagParsing = parsedValue
+		}
+	}
+
+	if filterParam := queryParams.Get("tag-filter"); filterParam != "" {
+		tagFilter = filterParam
+	}
+
+	// Here, add logic to use ws1URL, ws1Interval, tagParsing, and tagFilter
+	// to fetch and update metrics
 
 	// Serve the metrics
 	promhttp.Handler().ServeHTTP(w, r)
